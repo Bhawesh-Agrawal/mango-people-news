08ca7c5754f5bb0dc8c2f0981345a08321672e0f
Bhawesh-Agrawal <bhawesha4@gmail.com>
2026-03-21 12:42:20 +0530

    app route fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Hero from './components/home/Hero'
